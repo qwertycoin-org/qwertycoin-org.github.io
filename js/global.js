b034d22693ecdeb8c02de00891e2a8cf024f2d3b
nnian <48366474+nnian@users.noreply.github.com>
2020-09-01 14:07:40 +0200

    add getBrowserLanguage

--- js/global.js
+++ js/global.js
@@ -1,24 +1,3 @@
-var setCookie = function(name, value, days) {
-	var expires = "";
-	if (days) {
-		var date = new Date();
-		date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-		expires = "; expires=" + date.toUTCString();
-	}
-	document.cookie = name + "=" + (value || "") + expires + "; path=/";
-}
-
-var getCookie = function(name) {
-	var nameEQ = name + "=";
-	var ca = document.cookie.split(';');
-	for (var i = 0; i < ca.length; i++) {
-		var c = ca[i];
-		while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-	}
-	return null;
-}
-
 jQuery(function($) {
 	
 	var do_translate = function() {
